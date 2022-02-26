64c84aaa9f6ff8f334281fcb8c368cf9a8df5c5b
tomoprogrammer <train.vegacat@gmail.com>
2022-02-26 02:35:57 +0000

    class

---
+++ class2.php
@@ -0,0 +1,13 @@
+<?php
+class Human
+{
+    static $class_name = "Human";
+    
+    function init(){
+        $this->name = "大泉";
+    }
+    
+    public function show() {
+        print($this->name."\n");
+    }
+}
